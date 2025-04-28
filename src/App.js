93a0bfbd9a971f6f8f660c19f1e5e6f46b80470f
HarshMaurya04 <harshm8540@gmail.com>
2025-04-28 15:56:48 +0530

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Flip, toast, ToastContainer } from "react-toastify";
+import { LowerCh, NumberCh, SymbolCh, UpperCh } from "./data/PassChar";
 
 function App() {
+  let [uppercase, setUppercase] = useState(false);
+  let [lowercase, setLowercase] = useState(false);
+  let [number, setNumber] = useState(false);
+  let [symbol, setSymbol] = useState(false);
+  let [passlength, setPasslength] = useState(8);
+  let [showpass, setShowpass] = useState("");
+
+  let createPassword = () => {
+    let finalPassword = "";
+    let charSet = "";
+    if (uppercase || lowercase || number || symbol) {
+      if (uppercase) charSet += UpperCh;
+      if (lowercase) charSet += LowerCh;
+      if (number) charSet += NumberCh;
+      if (symbol) charSet += SymbolCh;
+
+      for (let i = 0; i < passlength; i++) {
+        finalPassword += charSet.charAt(
+          Math.floor(Math.random() * charSet.length)
+        );
+      }
+      setShowpass(finalPassword);
+    } else {
+      toast.error("Select at least one checkbox!");
+    }
+  };
+
+  let copyPass = () => {
+    if (showpass) {
+      navigator.clipboard
+        .writeText(showpass)
+        .then(() => {
+          toast.success("Password copied to clipboard!");
+        })
+        .catch((err) => {
+          console.error("Failed to copy:", err);
+        });
+    } else {
+      toast.info("Generate the password first!");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="passBox">
+        <h2 className="heading">Password Generator</h2>
+
+        <div className="inputField">
+          <input type="text" readOnly value={showpass} />
+          <button onClick={copyPass}>Copy</button>
+        </div>
+
+        <div className="passLength">
+          <label>Password length:</label>
+          <input
+            type="number"
+            max={20}
+            min={8}
+            value={passlength}
+            onChange={(event) => setPasslength(event.target.value)}
+          />
+        </div>
+
+        <div className="includeField">
+          <label>Include uppercase letters:</label>
+          <input
+            type="checkbox"
+            checked={uppercase}
+            onChange={() => setUppercase(!uppercase)}
+          />
+        </div>
+
+        <div className="includeField">
+          <label>Include lowercase letters:</label>
+          <input
+            type="checkbox"
+            checked={lowercase}
+            onChange={() => setLowercase(!lowercase)}
+          />
+        </div>
+
+        <div className="includeField">
+          <label>Include numbers:</label>
+          <input
+            type="checkbox"
+            checked={number}
+            onChange={() => setNumber(!number)}
+          />
+        </div>
+
+        <div className="includeField">
+          <label>Include symbols:</label>
+          <input
+            type="checkbox"
+            checked={symbol}
+            onChange={() => setSymbol(!symbol)}
+          />
+        </div>
+
+        <div className="genBtn">
+          <button onClick={createPassword}>Generate Password</button>
+        </div>
+      </div>
+
+      <ToastContainer transition={Flip} />
     </div>
   );
 }
---
+++ src/data/PassChar.jsx
@@ -0,0 +1,4 @@
+export let SymbolCh = "!@#$&.";
+export let UpperCh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export let LowerCh = "abcdefghijklmnopqrstuvwxyz";
+export let NumberCh = "0123456789";
